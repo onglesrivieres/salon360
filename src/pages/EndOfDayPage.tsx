ff729aa77492ab34bf437d624963606f42dec9b2
onglesrivieres <onglesrivieresqc@gmail.com>
2025-12-30 14:41:26 -0800

    Updated EndOfDayPage.tsx

--- src/pages/EndOfDayPage.tsx
+++ src/pages/EndOfDayPage.tsx
@@ -856,7 +856,7 @@ export function EndOfDayPage({ selectedDate, onDateChange }: EndOfDayPageProps) 
                   className="w-full"
                 >
                   <Vault className="w-3 h-3 mr-1" />
-                  Deposit into Safe
+                  Safe Drop
                 </Button>
               </div>
             </div>
